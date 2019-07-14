254258b92518041e9d603ec91050b6738df50768
Sawan Vithlani <sawan@kil-soft.com>
2019-07-14 17:43:01 +1000

    Package updates

---
+++ elpa/prescient-20190712.18/prescient-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "prescient" "20190712.18" "Better sorting and filtering" '((emacs "25.1")) :stars '(#("112" 0 3 (font-lock-face paradox-star-face))) :commit "a118a180dca1db4768d794a79469bed24c3cda23" :keywords '("extensions") :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :url "https://github.com/raxod502/prescient.el")
