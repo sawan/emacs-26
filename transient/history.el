5fbf81a094baac5c6897a9b5d6a420d7233be914
Sawan Vithlani <sawan@kil-soft.com>
2019-08-31 11:50:58 +1000

    Ignore things

--- transient/history.el
+++
@@ -1,9 +0,0 @@
-((magit-commit nil)
- (magit-dispatch nil)
- (magit-fetch nil)
- (magit-log
-  ("-n256" "--graph" "--decorate"))
- (magit-push nil
-	     ("--dry-run"))
- (magit-reset nil)
- (magit-run nil))
