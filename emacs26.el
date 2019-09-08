04b379707dd18f68e4bbe50b25a5af67b94420b5
Sawan Vithlani <sawan@kil-soft.com>
2019-09-08 16:46:10 +1000

    Holy shiiiiittttt.....

--- emacs26.el
+++ emacs26.el
@@ -90,6 +90,33 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; https://oremacs.com/2017/12/27/company-numbers/
+(setq company-show-numbers t)
+
+(let ((map company-active-map))
+  (mapc
+   (lambda (x)
+     (define-key map (format "%d" x) 'ora-company-number))
+   (number-sequence 0 9))
+  (define-key map " " (lambda ()
+                        (interactive)
+                        (company-abort)
+                        (self-insert-command 1)))
+  (define-key map (kbd "<return>") nil))
+
+(defun ora-company-number ()
+  "Forward to `company-complete-number'.
+
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+  (interactive)
+  (let* ((k (this-command-keys))
+         (re (concat "^" company-prefix k)))
+    (if (cl-find-if (lambda (s) (string-match re s))
+                    company-candidates)
+        (self-insert-command 1)
+      (company-complete-number (string-to-number k)))))
+
 
 (require 'revbufs)
 (require 'magit)
