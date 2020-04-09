283f6fb7158531b51787e53e860133e062cabe16
Sawan Vithlani <sawanvithlani@Lyrae.local>
2020-04-09 16:09:03 +1000

    Package updates.

---
+++ elpa/ivy-prescient-20200407.1906/ivy-prescient-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ivy-prescient" "20200407.1906" "prescient.el + Ivy" '((emacs "25.1") (prescient "4.1") (ivy "0.11.0")) :commit "3be69ac0d9d4e5dd5596673ce5dfebc31d8662ca" :keywords '("extensions") :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :url "https://github.com/raxod502/prescient.el")
--- elpa/posframe-20200318.2254/posframe-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "posframe" "20200318.2254" "Pop a posframe (just a frame) at point" '((emacs "26")) :commit "13d756d6a6614e934316be92cee2eb10bc6d4b3f" :keywords '("convenience" "tooltip") :authors '(("Feng Shu" . "tumashu@163.com")) :maintainer '("Feng Shu" . "tumashu@163.com") :url "https://github.com/tumashu/posframe")
