43d5fa1b9d81f1c51c4f2f22089093feb13b8e19
Sawan Vithlani <sawanvithlani@Lyrae.local>
2020-04-26 13:48:47 +1000

    Package updates.

--- elpa/overcast-theme-20190326.1827/overcast-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "overcast-theme" "20190326.1827" "A dark but vibrant color theme for Emacs" '((emacs "24")) :stars '(#("7" 0 1 (font-lock-face paradox-star-face))))
---
+++ elpa/overcast-theme-20200425.1601/overcast-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "overcast-theme" "20200425.1601" "A dark but vibrant color theme for Emacs" '((emacs "24")) :commit "e02b835a08919ead079d7221d513348ac02ba92e" :keywords '("theme") :authors '(("Mohammed Ismail Ansari" . "team.terminal@gmail.com")) :maintainer '("Mohammed Ismail Ansari" . "team.terminal@gmail.com") :url "http://ismail.teamfluxion.com")
