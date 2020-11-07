f3d6576120018530a0b1b327977bda00016e9147
Sawan Vithlani <sawan@kil-soft.com>
2020-11-07 18:22:31 +1100

    Buffer expose.

---
+++ elpa/buffer-expose-0.4.3/buffer-expose-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "buffer-expose" "0.4.3" "Visual buffer switching using a window grid" '((emacs "25") (cl-lib "0.5")) :stars '("") :keywords '("convenience") :authors '(("Clemens Radermacher" . "clemera@posteo.net")) :maintainer '("Clemens Radermacher" . "clemera@posteo.net") :url "https://github.com/clemera/buffer-expose")
