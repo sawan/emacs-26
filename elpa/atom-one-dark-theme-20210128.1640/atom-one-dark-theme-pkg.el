07e956949074ee4b8cb97666bde9f3569b6aaa21
Sawan Vithlani <sawan@kili-soft.com>
2021-02-15 11:51:13 +1100

    Package updates.

---
+++ elpa/atom-one-dark-theme-20210128.1640/atom-one-dark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/sawanvithlani/.emacs.d/elpa/atom-one-dark-theme-20210128.1640/atom-one-dark-theme.el  -*- no-byte-compile: t -*-
+(define-package "atom-one-dark-theme" "20210128.1640" "Atom One Dark color theme" 'nil :stars '("") :commit "b34b62e85593812b55ee552a1cb0eecfb04767bb" :authors '(("Jonathan Chu" . "me@jonathanchu.is")) :maintainer '("Jonathan Chu" . "me@jonathanchu.is") :url "https://github.com/jonathanchu/atom-one-dark-theme")
---
+++ elpa/slime-20210202.1426/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20210202.1426" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5")
+    (macrostep "0.9"))
+  :commit "f135f69c5c97bb1f2456d05ee1e84ad6b4495ca3" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
