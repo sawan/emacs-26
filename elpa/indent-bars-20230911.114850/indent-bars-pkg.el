1a83cbb0229b0b39b539ca8411ba7e7341d22593
Sawan Vithlani <Sawan.Vithlani@cba.com.au>
2023-09-19 10:30:14 +1000

    Check in package.

---
+++ elpa/indent-bars-20230911.114850/indent-bars-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from indent-bars.el  -*- no-byte-compile: t -*-
+(define-package "indent-bars" "20230911.114850" "highlight indentation with bars" '((emacs "27.1") (compat "29.1.4.1")) :keywords '("convenience") :url "https://github.com/jdtsmith/indent-bars")
